b0aa519fa713d28fc3478b9b71baa30c526e2f0a
zanderlx <lsaplan97@gmail.com>
2019-09-29 01:42:16 -0700

    Initial commit

---
+++ Backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "<h1>Welcome to main page for the SeriousAndProGamers API v1.0! (Work In Progress...)</h1><br><p>Use the base url followed by <strong>/v1/&lt;route_path&gt;</strong> to test the API!<p><br><p>Use <strong>/v1</strong> to check the available routes</p>"
+
+
+@app.route("/v1")
+def avail_routes():
+    return "<p>Available routes:<br> GET /ping -- sanity check endpoint</p>"
+
+
+@app.route("/v1/ping")
+def ping():
+    return "pong!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
