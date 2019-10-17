ccbb141b683291ca8e7bdd98bd3243ba2a50238d
zanderlx <lsaplan97@gmail.com>
2019-10-16 21:46:05 -0700

    Updating pathing of deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,7 @@ echo -e "Host $server_address\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add .travis/deploy_key
 ssh -i .travis/deploy_key lx_zander2@www.seriousandprogamers.com <<EOF
     cd spgsite-webapp && git pull
-    cd spgsite-webapp/Frontend
+    cd Frontend
     npm install
     npm run build
     rm -r /web-app/Frontend/*
