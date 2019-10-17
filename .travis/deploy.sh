b3ca673826ba0fa74f4d82eb59ab300cb6d53a83
zanderlx <lsaplan97@gmail.com>
2019-10-16 21:55:48 -0700

    Updated production frontend directory path

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,9 +6,9 @@ echo -e "Host $server_address\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add .travis/deploy_key
 ssh -i .travis/deploy_key lx_zander2@www.seriousandprogamers.com <<EOF
     cd spgsite-webapp && git pull
-    cd Frontend
+    cd frontend
     npm install
     npm run build
-    rm -r /web-app/Frontend/*
-    rsync -r dist/* /web-app/Frontend/
+    rm -r /web-app/frontend/*
+    rsync -r dist/* /web-app/frontend/
 EOF
