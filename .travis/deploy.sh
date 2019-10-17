cf22761759832abeb4baee3a5aa3dc74ec084c4b
zanderlx <lsaplan97@gmail.com>
2019-10-16 21:43:33 -0700

    Updated deploy script to pull latest changes

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,9 +4,8 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key
 echo -e "Host $server_address\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add .travis/deploy_key
-git remote add spgsite-webapp lx_zander2@www.seriousandprogamers.com:/home/lx_zander2/spgsite-webapp.git
-git push spgsite-webapp master
 ssh -i .travis/deploy_key lx_zander2@www.seriousandprogamers.com <<EOF
+    cd spgsite-webapp && git pull
     cd spgsite-webapp/Frontend
     npm install
     npm run build
