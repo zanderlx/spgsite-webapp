2582ced513ac9f4b0dc199e9ca4ed208369c5441
zanderlx <lsaplan97@gmail.com>
2019-10-16 21:33:52 -0700

    Updated page for testing continuous development

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to your Web App"/>
+    <HelloWorld msg="Welcome to the SPG Site"/>
   </div>
 </template>
 
